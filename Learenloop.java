0e631cb5cad04d6748f48db09a1c680cda4dcd48
Saimaheshcj <104827651+Saimaheshcj@users.noreply.github.com>
2022-05-04 17:29:42 +0530

    Add files via upload

---
+++ Learenloop.java
@@ -0,0 +1,13 @@
+package week1.day3;
+
+public class Learenloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i=3; i<=7; i++) {
+System.out.println(i);
+
+	}	
+
+	}
+}
---
+++ Twowheeler.java
@@ -0,0 +1,22 @@
+package sai.week.day1;
+
+public class Twowheeler {
+int noOfwheels=2;
+short noOfmirror=2;
+long chassisNumber=133244567890l;
+boolean isPunchered=true;
+String bikeName = "ktm";
+double runningKM=2.500;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Twowheeler object= new Twowheeler();
+System.out.println(object.noOfwheels);
+System.out.println(object.noOfmirror);
+System.out.println(object.chassisNumber);
+System.out.println(object.isPunchered);
+System.out.println(object.bikeName);
+System.out.println(object.runningKM);
+	}
+
+}
